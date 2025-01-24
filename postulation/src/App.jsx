14e73ea9689dd02b41f40724e53f0f332881f4ba
Julian Torres Mendioroz <juliantorresmendioroz348@gmail.com>
2025-01-24 19:45:09 -0300

    v0.1 - Deployed (some changes)

--- postulation/src/App.jsx
+++ postulation/src/App.jsx
@@ -1,13 +1,13 @@
 import Home from './components/home/Home.jsx'
 import Subscribe from './components/Subscribe/Subscribe.jsx'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import AboutProject from './components/aboutproject/AboutProject.jsx';
 import './App.css'
 
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename="/EquipoDotGuru-Postulation">
      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/subscribe" element={<Subscribe />} />
--- postulation/src/components/home/Home.jsx
+++ postulation/src/components/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Footer from '../footer/Footer.jsx';
+import { Link } from "react-router-dom";
 import './Home.css';
 
 const Home = () => {
@@ -7,15 +8,11 @@ const Home = () => {
   return (
     <>
       <div className="div-home">
-        <h1 className="text-home">Do you want to be part of Equipodot Guru?</h1>
+        <h1 className="text-home">Do you want to be part of EquipoDot Guru?</h1>
         <p className="slogan-home">Click below to subscribe and become part of our community.</p>
         <div className="buttons-home">
-        <a href="/subscribe" className="button-home">
-          SUBSCRIBE HERE
-        </a>
-        <a href="/about" className="button-home">
-        ABOUT THE PROJECT
-        </a>
+        <Link to="/subscribe" className="button-home">SUBSCRIBE HERE</Link>
+        <Link to="/about" className="button-home">ABOUT THE PROJECT</Link>
         </div>
       </div>
       <Footer />
